120d1d783a99b9b7e1f1179d64c1efbd2ce9a628
kohct1 <kohct2006@gmail.com>
2024-05-07 22:42:44 +0800

    finished assignment #1

--- app/src/main/java/sg/edu/np/mad/madpractical4/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/ListActivity.java
@@ -12,8 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -28,30 +32,25 @@ public class ListActivity extends AppCompatActivity {
             return insets;
         });
 
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
-        arrayList.add("a");
+        ArrayList<User> users = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+            int randNum = rand.nextInt(9999999);
+            String name = "Name " + randNum;
+            String description = "Description " + randNum;
 
+            User user = new User(name, description, randNum, false);
+            user.setName("Name " + randNum);
+            user.setDescription("Description " + randNum);
+            user.setId(randNum);
+            users.add(user);
+        }
 
-
-
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        UserAdapter userAdapter = new UserAdapter(users, this);
+        LinearLayoutManager userLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(userLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(userAdapter);
     }
 }
---
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -0,0 +1,68 @@
+package sg.edu.np.mad.madpractical4;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+    private ArrayList<User> listObjects;
+
+    private ListActivity activity;
+
+    public UserAdapter(ArrayList<User> listObjects, ListActivity activity) {
+        this.listObjects = listObjects;
+        this.activity = activity;
+    }
+
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_activity_list, parent, false);
+        UserViewHolder holder = new UserViewHolder(view);
+        return holder;
+    }
+
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        User listItem = listObjects.get(position);
+        holder.name.setText(listItem.getName());
+        holder.description.setText(listItem.getDescription());
+
+        holder.smallImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+
+                builder.setTitle("Profile")
+                        .setMessage(listItem.getName())
+                        .setPositiveButton("Close", (dialog, which) -> {
+                            dialog.dismiss();
+                        })
+                        .setNegativeButton("View", (dialog, which) -> {
+                            Intent intent = new Intent(v.getContext(), MainActivity.class);
+
+                            intent.putExtra("id", listItem.getId());
+
+                            intent.putExtra("name", listItem.getName());
+
+                            intent.putExtra("description", listItem.getDescription());
+
+                            intent.putExtra("followed", listItem.getFollowed());
+
+                            v.getContext().startActivity(intent);
+                        });
+                builder.create().show();
+            }
+        });
+
+        String userName = listItem.name;
+        if (userName.endsWith("7")) {
+            holder.bigImage.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public int getItemCount() { return listObjects.size(); }
+}
